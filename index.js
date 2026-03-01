6df0e71f6e3a09b52a240d6299c729d696ca923f
Lataj2011 <latajdmitro2011@gmail.com>
2026-03-01 16:26:18 +0200

    2121

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ let http = require('http')
 let fs = require('fs')
 let path = require('path')
 
-let server = http.createServer(function () {
-    responce.end("hello world!")
-})
+let server = http.createServer(function (request, response) { 
+  response.end("hello world!");
+});
 
+server.listen(3000)
